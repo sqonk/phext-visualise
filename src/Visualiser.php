21f788bd1706e9887d5a4ae52e1c0945371de575
Theo Howell <sqonk@sqonk.com.au>
2021-07-18 19:31:13 +1000

    Type hint on quitCallback.

--- src/Visualiser.php
+++ src/Visualiser.php
@@ -52,7 +52,7 @@ class Visualiser
 	protected bool $alive = false;
 	protected string $inboundBuffer = "";
     protected string $pathPrefix = '';
-    protected $quitCallback;
+    protected ?callable $quitCallback = '';
     protected array $registeredWindows = [];
     
     protected const NEW_WINDOW = 1;
